574b79b675a5eb4b9925101ff7cef25b0155dafb
Michelle <mchristine.stone91@gmail.com>
2022-03-03 00:40:03 -0500

    Set up app to be converted to a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 // Create the main configuration object.
 // Write objects within this objec that tell webpack what to do.
@@ -55,6 +56,28 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    // new invokes a constructor function.
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      // Homepage for the PWA relative to the location of the manifest file.
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      // Specific to manifest plugin.
+      fingerprints: false,
+      // Determines whether the link to the manifest.json is added to the HTML.
+      inject: false,
+      // Array of objects.
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
 };
