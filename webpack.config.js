45f0f04d4fa52cb237123296cbac33af867a131e
Michelle <mchristine.stone91@gmail.com>
2022-03-02 22:31:19 -0500

    modularized code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // Create the main configuration object.
 // Write objects within this objec that tell webpack what to do.
@@ -7,19 +9,51 @@ const webpack = require("webpack");
 // Three basic properties: entry, output and mode.
 module.exports = {
   // Take code, bundle it and put it into folder we specify.
-  entry: "./assets/js/script.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
   },
-  //   Insdide array, tell webpack which plugin we want to use.
+  // Build step creates a series of bundled files, one for each listing in entry object.
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
+  },
+
+  //   Inside array, tell webpack which plugin we want to use.
   //   providePuglin to define jQuery variables.
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
   ],
-  // Mode in which we want webpack to run. Default: production.
-  mode: "development",
 };
